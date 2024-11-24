23da8bd733ec801f8d14467fe5f52dab00453383
panda <duremarkastu@gmail.com>
2024-11-24 19:51:25 +0300

    added room, guest and booking models/tabels with r/s defined

--- management/booking/forms.py
+++ management/booking/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import Booking
 
-
+"""
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['room', 'guest_name', 'check_in_date', 'check_out_date']
+        """
--- management/booking/models.py
+++ management/booking/models.py
@@ -1,22 +1,65 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 
 class Room(models.Model):
-    room_number = models.CharField(max_length=10, unique=True)
+    ROOM_TYPE_CHOICES = [
+        ('Single', 'Single'),
+        ('Double', 'Double'),
+    ]
+
+    ROOM_STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('Booked', 'Booked'),
+        ('Maintenance', 'Maintenance'),
+    ]
+
+    room_number = models.CharField(max_length=10, primary_key=True)
+    room_type = models.CharField(
+        max_length=20, choices=ROOM_TYPE_CHOICES, default='Single')
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    available = models.BooleanField(default=True)
+    room_status = models.CharField(
+        max_length=15, choices=ROOM_STATUS_CHOICES, default='Available')
 
     def __str__(self):
         return f"Room {self.room_number}"
 
+    class Meta:
+        ordering = ['room_number']  # sort by room number
 
-class Booking(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    guest_name = models.CharField(max_length=100)
-    check_in_date = models.DateField()
-    check_out_date = models.DateField()
+
+class Guest(models.Model):
+    phone_number = models.CharField(max_length=15, primary_key=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(blank=True, null=True)
 
     def __str__(self):
-        return f"Booking for {self.guest_name} in Room {self.room.room_number}"
+        return f"{self.first_name} {self.last_name} {self.phone_number}"
+
+    class Meta:
+        ordering = ['first_name']  # sort by first name
+
+
+class Booking(models.Model):
+    BOOKING_STATUS_CHOICES = [
+        ('Confirmed', 'Confirmed'),
+        ('Pending', 'Pending'),
+        ('Cancelled', 'Cancelled'),
+    ]
+
+    booking_id = models.AutoField(primary_key=True)
+    room = models.ForeignKey(
+        Room, on_delete=models.CASCADE, related_name='bookings')
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE, null=True,
+                              related_name='bookings')
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    booking_date = models.DateTimeField(default=datetime.datetime.now)
+    status = models.CharField(
+        max_length=10, choices=BOOKING_STATUS_CHOICES, default='Pending')
+
+    def __str__(self):
+        return f"Booking {self.booking_id}: for {self.guest.first_name} {self.guest.last_name}: in Room {self.room.room_number}"
