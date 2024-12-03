0de202316f565b0662736cfcc25bc621d539790c
panda <duremarkastu@gmail.com>
2024-12-03 13:17:55 +0300

    added view to link book room and db but not functioning

--- management/booking/forms.py
+++ management/booking/forms.py
@@ -1,9 +1,30 @@
 from django import forms
 from .models import Booking
 
-"""
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['room', 'guest_name', 'check_in_date', 'check_out_date']
-        """
+        fields = ['room', 'guest', 'check_in_date', 'check_out_date',
+                  'payment_method', 'bank_name', 'transaction_id']
+
+        PAYMENT_CHOICE = [
+            ('cash', 'cash'),
+            ('card', 'card'),
+            ('mobile banking', 'mobile banking'),
+        ]
+
+        payment_method = forms.ChoiceField(
+            choices=PAYMENT_CHOICE, required=True)
+        bank_name = forms.CharField(required=True)
+        transaction_id = forms.CharField(required=True)
+
+        def clean(self):
+            cleaned_data = super().clean()
+            payment_method = cleaned_data.get('payment_method')
+
+            if payment_method == 'mobile-banking':
+                if not cleaned_data('bank_name') or not cleaned_data.get('transaction_id'):
+                    raise forms.ValidationError(
+                        "Bank Name and Transaction Id are required for mobile banking payment.")
+            return cleaned_data
--- management/booking/views.py
+++ management/booking/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required, user_passes_test
-from .models import Room, Booking
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from .models import Room, Guest, Booking
+from .forms import BookingForm
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def book_room(request):
+    if request.method == 'POST':
+        guest_name = request.POST.get('guest_name')
+        phone_number = request.POST.get('contact_number')
+        check_in_date = request.POST.get('check_in_date')
+        check_out_date = request.POST.get('check_out_date')
+        room_number = request.POST.get('room_number')
+        payment_method = request.POST.get('payment_method')
+        bank_name = request.POST.get('bank_name')
+        transaction_id = request.POST.get('transaction_id')
+
+        # Get Room and Guest Objects
+        try:
+            room = Room.objects.get(room_number=room_number)
+            guest, created = Guest.objects.get_or_create(
+                phone_number=phone_number,
+                default={'first_name': guest_name.split(
+                )[0], 'last_name': ' '.join(guest_name.split()[1:])}
+            )
+
+            # Booking instance
+            booking = Booking(
+                room=room,
+                guest=guest,
+                check_in_date=check_in_date,
+                check_out_date=check_out_date,
+                payment_method=payment_method,
+                bank_name=bank_name if payment_method == 'Mobile Banking' else None,
+                transaction_id=transaction_id if payment_method == 'Mobile Banking' else None
+            )
+
+        # Validate and sabe booking
+            try:
+                booking.full_clean()
+                booking.save()
+                messages.success(request, "Booking Successfull")
+                return redirect('booking_confirmation')
+            except ValidationError as e:
+                messages.error(request, f"Error: {e}")
+                return redirect('booking_main')
+        except Room.DoesNotExist:
+            messages.error(request, "Room not Found!")
+            return redirect('booking_main')
+    else:
+        available_rooms = Room.objects.filter(room_status='Available')
+        return render(request, 'booking_main.html', {'rooms': available_rooms})
 
 
 def is_receptionist(user):
