544808ac75695c204aeade0050298c180fdc69d2
panda <duremarkastu@gmail.com>
2025-01-31 23:26:18 +0300

    ui: added separate bookings and payments tabs

--- management/booking/static/booking/js/booking_main.js
+++ management/booking/static/booking/js/booking_main.js
@@ -2,71 +2,95 @@
 const currentDate = new Date();
 
 const formattedDate = currentDate.toLocaleDateString('en-US', {
-	weekday: 'long',
-	year: 'numeric',
-	month: 'long',
-	day: 'numeric'
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
 });
 
 document.getElementById('current-date').textContent = formattedDate;
 
 //show the Home section
 function showHome() {
-    hideAllSections(); 
-    document.getElementById('home-section').style.display = 'block';
-    document.getElementById('info-section').style.display = 'block';
+  hideAllSections();
+  document.getElementById('home-section').style.display = 'block';
+  document.getElementById('info-section').style.display = 'block';
+}
+
+//show bookings section
+function showBookings() {
+  hideAllSections();
+  document.getElementById('bookings-').style.display = 'block';
 }
 
 // show the Book Room section
 function showBookRoom() {
-    hideAllSections(); 
-    document.getElementById('book-room-section').style.display = 'block';
+  hideAllSections();
+  document.getElementById('book-room-section').style.display = 'block';
+}
+
+//shoe payments section
+function showPayment() {
+  hideAllSections();
+  document.getElementById('payments-section').style.display = 'block';
 }
 
 //show view rooms section
 function showViewRooms() {
-	hideAllSections();
-	document.getElementById('view-rooms-section').style.display = 'block';
+  hideAllSections();
+  document.getElementById('view-rooms-section').style.display = 'block';
 }
 
 // Function to hide all sections
 function hideAllSections() {
-    document.getElementById('home-section').style.display = 'none';  // Hide Home section
-    document.getElementById('info-section').style.display = 'none';  // Hide Info section
-    document.getElementById('book-room-section').style.display = 'none';  // Hide Book Room section
-    document.getElementById('view-rooms-section').style.display = 'none'; // Hide View Rooms section
+  document.getElementById('home-section').style.display = 'none';  // Hide Home section
+  document.getElementById('info-section').style.display = 'none';  // Hide Info section
+  document.getElementById('book-room-section').style.display = 'none';  // Hide Book Room section
+  document.getElementById('view-rooms-section').style.display = 'none'; // Hide View Rooms section
+  document.getElementById('bookings-section').style.display = 'none';
+  document.getElementById('payments-section').style.display = 'none';
 }
 
 // Event Listeners for navigation links
-document.getElementById('home-link').addEventListener('click', function (event) {
-    event.preventDefault();
-    showHome();
+document.getElementById('home-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  showHome();
 });
 
-document.getElementById('book-room-link').addEventListener('click', function (event) {
-    event.preventDefault();
-    showBookRoom();
+document.getElementById('book-room-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  showBookRoom();
 });
 
-document.getElementById('view-rooms-link').addEventListener('click', function (event) {
-    event.preventDefault();
-    showViewRooms();
+document.getElementById('view-rooms-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  showViewRooms();
+});
+
+document.getElementById('bookings-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  showBookings();
+});
+
+document.getElementById('payments-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  showPayment();
 });
 
 
 //Dynamic section for mobile banking payment method
 document.getElementById("payment-method").addEventListener("change", function() {
-	const paymentMethod = this.value;
-	const mobileBankingFields = document.getElementById("mobile-banking-fields");
-	
-	if ("mobile-banking" === paymentMethod){
-		mobileBankingFields.style.display = "block";
-	} else {
-		mobileBankingFields.style.display = "none";
-	}
+  const paymentMethod = this.value;
+  const mobileBankingFields = document.getElementById("mobile-banking-fields");
 
-	document.getElementById("bank-name").value = ""; 
-	document.getElementById("transaction-id").value = "";
+  if ("mobile-banking" === paymentMethod) {
+    mobileBankingFields.style.display = "block";
+  } else {
+    mobileBankingFields.style.display = "none";
+  }
+
+  document.getElementById("bank-name").value = "";
+  document.getElementById("transaction-id").value = "";
 });
 
 // Set the default section to show (Home) when the page loads
