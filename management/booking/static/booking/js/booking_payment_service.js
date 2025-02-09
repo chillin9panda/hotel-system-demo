f484062182cf522d8ec4ee1aaa78d694578b96ea
panda <duremarkastu@gmail.com>
2025-02-09 12:14:17 +0300

    added check in checkout ui

--- management/booking/static/booking/js/booking_payment_service.js
+++ management/booking/static/booking/js/booking_payment_service.js
@@ -1,30 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-  function toggleMenu(event, bookingId) {
+  function toggleMenu(event, menuId) {
     event.preventDefault();
-    let menu = document.getElementById("menu_" + bookingId);
+    let menu = document.getElementById(menuId);
 
     document.querySelectorAll('.booking-menu').forEach(m => {
-      if (m !== menu)
-        m.style.display = 'none';
+      if (m !== menu) {
+        m.style.display = "none";
+      }
     });
 
-    menu.style.display = menu.style.display === "block" ? "none" : "block"
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
   }
 
-  document.querySelectorAll(".edit-booking-link").forEach(link => {
-    link.addEventListener("click", function(event) {
-      let bookingId = this.getAttribute("data-booking-id");
-      toggleMenu(event, bookingId);
+
+  function addMenuToggleListener(selector, menuPrefix) {
+    document.querySelectorAll(selector).forEach(link => {
+      link.addEventListener("click", function(event) {
+        let bookingId = this.getAttribute("data-booking-id");
+        toggleMenu(event, `${menuPrefix}${bookingId}`);
+      });
     });
-  });
+  }
+
+  addMenuToggleListener(".edit-booking-link", "booking-menu_");
+  addMenuToggleListener(".booking-status-link", "status-menu_");
+
+
 
   document.addEventListener("click", function(event) {
-    let isClickInside = event.target.closest(".edit-booking-link, .booking-menu");
-
-    if (!isClickInside) {
+    if (!event.target.closest(".edit-booking-link, .booking-status-link, .booking-menu")) {
       document.querySelectorAll(".booking-menu").forEach(menu => {
         menu.style.display = "none";
       });
     }
   });
 });
+
