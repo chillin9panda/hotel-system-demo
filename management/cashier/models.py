f107add60a508350141cd97d7a261b23e4c1f8b6
panda <duremarkastu@gmail.com>
2025-02-17 22:13:01 +0300

    added items model

--- management/cashier/models.py
+++ management/cashier/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    ITEM_TYPE_CHOICES = [
+        ('Food', 'Food'),
+        ('Drinks', 'Drinks'),
+    ]
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=30, null=False)
+    item_type = models.CharField(
+        max_length=15, choices=ITEM_TYPE_CHOICES, null=False)
+    item_price = models.DecimalField(max_digits=8, decimal_places=2)
