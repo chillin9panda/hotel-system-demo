645074e2c12a29cbe26c696f04513e2461103238
panda <duremarkastu@gmail.com>
2025-02-18 19:57:48 +0300

    orders table

--- management/cashier/models.py
+++ management/cashier/models.py
@@ -13,3 +13,20 @@ class Item(models.Model):
     item_type = models.CharField(
         max_length=15, choices=ITEM_TYPE_CHOICES, null=False)
     item_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    item = models.ForeignKey(
+        Item,  on_delete=models.CASCADE, related_name='orders')
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='orders')
+    quantity = models.PositiveIntegerField(null=False, default=1)
+
+    @property
+    def total_price(self):
+        return self.quantity * self.item.item_price
